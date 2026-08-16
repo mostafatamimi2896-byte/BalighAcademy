b953eabd982b7eb61e06ac646241967763cd8e26
mostafatamimi2896-byte <mostafatamimi2896@gmail.com>
2026-08-16 12:20:24 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BalighAcademy;
+using BalighAcademy.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
@@ -9,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<StudentService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
