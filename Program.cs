68fca87c8f867e504543ed4aeec1939dfb82940c
mostafatamimi2896-byte <mostafatamimi2896@gmail.com>
2026-08-16 08:59:46 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using BalighAcademy;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
